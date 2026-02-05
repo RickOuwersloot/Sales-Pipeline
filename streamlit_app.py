e6154538f858ce646d55f5a5112ae526af3287a9
RickOuwersloot <info@romarketing.nl>
2026-02-05 14:18:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 # --- 1. CONFIGURATIE ---
-st.set_page_config(page_title="RO Marketing Pipeline", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="RO Marketing Pipeline", page_icon="Logo RO Marketing.png", layout="wide", initial_sidebar_state="expanded")
 
 # --- 2. CSS STYLING (THE BLUE FORCE FIX) ---
 st.markdown("""
